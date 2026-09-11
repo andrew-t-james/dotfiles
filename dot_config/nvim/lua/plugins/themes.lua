41cc856772cbb920f1a8e376bda7eea0cd3b7c74
Andrew James <ajames20@gmail.com>
2026-09-11 12:50:05 -0500

    fix(nvim): replace unavailable Monokai plugin source

--- dot_config/nvim/lua/plugins/themes.lua
+++ dot_config/nvim/lua/plugins/themes.lua
@@ -3,7 +3,7 @@ return {
   { "EdenEast/nightfox.nvim", lazy = true },
   { "ellisonleao/gruvbox.nvim", lazy = true },
   { "rebelot/kanagawa.nvim", lazy = true },
-  { "gthelding/monokai-pro.nvim", lazy = true },
+  { "loctvl842/monokai-pro.nvim", lazy = true },
   { "rose-pine/neovim", name = "rose-pine", lazy = true },
   { "neanias/everforest-nvim", lazy = true },
   { "ribru17/bamboo.nvim", lazy = true },
